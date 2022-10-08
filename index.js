d5217673b310759570a4c6147fd8acd55fbd1a97
Camila Martinez <camilamarb98@gmail.com>
2022-10-08 11:45:06 +0200

    add db connection

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log("Connected to MongoDB.");
+    } catch (error) {
+        throw error;
+    }
+};
+
 
 app.listen(8800, () => {
+    connect();
     console.log("Connected to backend.");
-})
+});
