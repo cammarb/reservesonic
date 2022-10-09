58030716437d72ead284b1c814cf700c523de227
Camila Martinez <camilamarb98@gmail.com>
2022-10-09 12:37:36 +0200

    add Room, Studios, Users endpoints

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
 import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import studiosRoute from "./routes/studios.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -19,7 +23,10 @@ const connect = async () => {
 //     res.send("Hello first request!");
 // })
 
-app.use("/auth", authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/studios", studiosRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
