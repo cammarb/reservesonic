998e4783aad7d4324f32d98e69f40ab5e4a7dd18
Camila Martinez <camilamarb98@gmail.com>
2022-10-22 22:42:32 +0200

    add Create studio route

--- api/routes/studios.js
+++ api/routes/studios.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+// Create
+router.post("/", (req, res) => {
+
+})
+
+
 router.get("/", (req, res) => {
     res.send("This is the studios endpoint.");
 });
