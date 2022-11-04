a3cfc89a7bb923d58d4ed5bc74c395e5ff9a16a0
Camila Martinez <camilamarb98@gmail.com>
2022-11-04 17:53:16 +0100

    get latest change when updating

--- api/routes/studios.js
+++ api/routes/studios.js
@@ -22,7 +22,18 @@ router.post("/", async (req, res) => {
 // UPDATE
 router.put("/:id", async (req, res) => {
     try {
-        const updateStudio = await Studio.findByIdAndUpdate(req.params.id, { $set: req.body })
+        const updateStudio = await Studio.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updateStudio);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE
+router.put("/:id", async (req, res) => {
+    try {
+        const deleteStudio = await Studio.findByIdAndDelete(req.params.id, { $set: req.body })
         res.status(200).json(updateStudio);
     }
     catch (err) {
