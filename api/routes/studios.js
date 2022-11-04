66814308dabca969b224a1ee54501e2970fdf72e
Camila Martinez <camilamarb98@gmail.com>
2022-11-04 17:49:38 +0100

    add CREATE and UPDATE function to Studios

--- api/routes/studios.js
+++ api/routes/studios.js
@@ -1,10 +1,33 @@
 import express from "express";
+import Studio from "../models/Studio.js";
 
 const router = express.Router();
 
 // Create
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 
+    const newStudio = new Studio(req.body);
+
+    try {
+        const savedStudio = await newStudio.save();
+        res.status(200).json(savedStudio);
+
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        const updateStudio = await Studio.findByIdAndUpdate(req.params.id, { $set: req.body })
+        res.status(200).json(updateStudio);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 
