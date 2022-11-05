5e3aa2ec12e6c1e1028d7e2378771e6e8a67eb62
Camila Martinez <camilamarb98@gmail.com>
2022-11-05 18:00:21 +0100

    add auth controller | register, login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,10 +1,15 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
-            password: req.body.password,
+            password: hash,
             name: req.body.name,
             age: req.body.age,
             email: req.body.email,
@@ -14,7 +19,33 @@ export const register = async (req, res, next) => {
         await newUser.save();
 
         res.status(200).send("User has been created.");
-        // res.send("This is the auth endpoint.");
+    }
+    catch (err) {
+        next(err);
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+
+        if (!user) {
+            return next(createError(404, "User was not found."));
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!isPasswordCorrect) {
+            return next(createError(404, "Wrong username or password."));
+
+        }
+
+        const { password, isAdmin, ...otherDetails } = user;
+        res.status(200).json({ ...otherDetails });
     }
     catch (err) {
         next(err);
