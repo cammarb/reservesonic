f6ec29973c6b30491608de793c72759f8ee89e4b
Camila Martinez <camilamarb98@gmail.com>
2022-11-05 15:34:35 +0100

    add User model

---
+++ api/models/User.js
@@ -0,0 +1,33 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    },
+},
+    { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
