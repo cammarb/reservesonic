f5e6e8eda03dc4bc3b786126c9ff7d5f013a4121
Camila Martinez <camilamarb98@gmail.com>
2022-11-05 18:33:28 +0100

    add User CRUD

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("This is the user endpoint.");
-});
+// CREATE
+router.post("/", createUser);
+
+// UPDATE
+router.put("/:id", updateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getAllUsers);
 
 export default router;
