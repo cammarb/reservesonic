0d3ca43addfeca8c22812246e3ddd73a484d9722
Camila Martinez <camilamarb98@gmail.com>
2022-11-05 15:35:27 +0100

    add registratiion of users route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("This is the auth endpoint.");
-});
+router.get("/", register);
 
 export default router;
