1c32738ddf24ff8b12567646bfd52252fa475291
Camila Martinez <camilamarb98@gmail.com>
2022-11-10 11:49:59 +0100

    update Room model

--- api/models/Room.js
+++ api/models/Room.js
@@ -2,27 +2,37 @@ import mongoose from 'mongoose';
 
 // const { Schema } = mongoose;
 
-const RoomSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+const RoomSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        capacity: {
+            type: Number,
+            required: true
+        },
+        maxPeople: {
+            type: Number,
+            required: true
+        },
+        photos: {
+            type: [String],
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        unavailableDates: {
+            type: [Date]
+        }
     },
-    type: {
-        type: String,
-        required: true
-    },
-    capacity: {
-        type: Number,
-        required: true
-    },
-    photos: {
-        type: [String],
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-})
+    { timestamps: true }
+);
 
 export default mongoose.model("Room", RoomSchema);
