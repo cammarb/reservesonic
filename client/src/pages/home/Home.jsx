3a71605a06f340193293319b1183485e238a1226
Camila Martinez <camilamarb98@gmail.com>
2022-11-13 14:43:15 +0100

    update Home page structure

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../components/navbar/Navbar";
-// import Header from "../../components/header/Header";
+import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 
 import "./home.css";
@@ -8,12 +8,11 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            {/* <Header /> */}
+            <Header />
             <div className="homeContainer">
-                <h1 className="homeTitle">Browse by ...</h1>
-                <h1 className="homeTitle">...</h1>
-                <Footer />
+
             </div>
+            <Footer />
         </div>
     );
 };
